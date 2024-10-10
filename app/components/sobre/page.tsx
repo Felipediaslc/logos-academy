66189bce832c5b6225b438eedd640ecea9440635
felipediaslc <felipe.00.dias@gmail.com>
2024-10-10 15:35:06 -0300

    version-v19

--- app/components/sobre/page.tsx
+++ app/components/sobre/page.tsx
@@ -2,6 +2,12 @@
 import React from "react";
 import Image from "next/image";
 import Instrutor from "../../../public/danielcaval.png"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion"
 
 
 
@@ -34,16 +40,106 @@ font-mono">
 lg:text-3xl">
 Somos uma plataforma de aprendizado contínuo em programação
 </h2>
-<p className="mt-6 max-w-xs text-center text-sm text-gray-200 
+<p className="mt-6 max-w-xs  text-justify text-sm text-gray-200 
 lg:max-w-[43.375rem] lg:text-base">
 A Logos Academy, 
-é uma escola de programação que já transformou 
-a vida de mais de 55 mil alunos e alunas através
+é uma escola de programação que transforma  
+a vida de alunos e alunas através
 do ensino de tecnologia e programação.
-Junto a Digital House, reunimos a maior comunidade
-de devs da America Latina e temos o objetivo de impulsionar
-pessoas para o seu próximo nível, independente do seu momento
-de carreira.</p>
+Nosso objetivo é adquirir e desenvolver com as ferramentas,
+habilidades e conhecimentos técnicos necessários 
+para resolver situações que nos serão apresentadas na vida real.
+Também procuraremos aprender a (pensar como um programador),
+uma habilidade que inclui tudo, desde a montagem de um sistema até 
+a identificação de erros (bugs) no mesmo, propondo soluções e atualizações
+baseadas em mudanças na tecnologia. Procuramos incentivar constantemente
+a pesquisa, a busca por informações, a leitura de documentação técnica 
+e o trabalho em equipe.</p>
+<Accordion type="single" collapsible className="w-full flex flex-col justifi-center lg:pt-[2rem] lg:pl-[12rem]">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>O que vamos alcançar no curso?</AccordionTrigger>
+        <AccordionContent>
+          <ul>
+            <li>
+             <p className="text-justify">- Adquirir os conhecimentos necessários para desenvolver um site de ponta a ponta integrando front-end e back-end.</p>
+            </li>
+            <li>
+             <p className="text-justify">- Identificar os problemas em um projeto e implementar as modificações necessárias para resolvê-los.</p>
+            </li>
+            <li>
+             <p className="text-justify">- Trabalhar em equipe.</p>
+            </li>
+            <li>
+             <p className="text-justify">- Saber a que documentação e comunidades recorrer para a solução de problemas e manter-se atualizado em cada uma das linguagens aprendidas.</p>
+            </li>
+            <li>
+             <p className="text-justify">- Trabalhar com frameworks para ser capaz de gerar projetos escaláveis, seguros e eficientes.</p>
+            </li>
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>O que vamos aprender no curso?</AccordionTrigger>
+        <AccordionContent>
+        <div className="lg:max-w-[25.875rem]">
+        <div className="mt-6 w-full flex-1 border-b border-orange-500 pb-6 last:border-none">
+        <span className="text-sm font-cooper-bold text-orange-500 tracking-widest lg:text-base">Introdução ao curso</span>
+        <p className="mt-2 text-sm text-white lg:text-base">
+        Bem-vindo ao curso e à imersão nas novas ferramentas que utilizaremos ao longo do curso. Conheceremos o terminal, o Visual Studio Code e muitas outras ferramentas.</p>
+        </div>
+
+        <div className="mt-6 w-full flex-1 border-b border-orange-500  tracking-widest pb-6 last:border-none">
+        <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Introdução ao Node.js e JavaScript</span>
+        <p className="mt-2 text-sm text-white lg:text-base">
+        Vamos conhecer o potencial que esta linguagem nos oferece e experimentar a diversidade de aplicações que podem ser feitas graças ao ambiente de desenvolvimento do Node.js.</p>
+         </div>
+
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Metodologias de desenvolvimento</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Aprender metodologias e ferramentas de trabalho utilizadas na área de desenvolvimento, como Git e Scrum.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Como funciona a web</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Entender como um site é estruturado e como podemos acrescentar estilo e design a essa estrutura.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Express</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Desenvolveremos o conceito de Full Stack, reunindo o mundo do front end e back end. Para isso, utilizaremos o framework mais utilizado no ecossistema do Node.js.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Banco de dados</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Saberemos o que são bancos de dados e como podemos operá-los. Trabalharemos com o MySQL e o Sequelize.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">JavaScript front-end</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Acrescentaremos dinamismo e interatividade ao nosso front-end.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">APIs</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Vamos criar nossas próprias APIs e aprender como consumir APIs de terceiros.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">React</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Criaremos uma aplicação com React. Trabalharemos com componentes e ciclo de vida. E por fim, vamos unir tudo isso.</p>
+         </div>
+         <div className="mt-6 w-full flex-1 border-b border-orange-500 tracking-widest pb-6 last:border-none">
+         <span className="text-sm font-cooper-bold text-orange-500 lg:text-base">Projeto Integrador</span>
+         <p className="mt-2 text-sm text-white lg:text-base">
+         Aprender como fazer um deploy de um projeto de desenvolvimento e aplicar no seu projeto integrador.</p>
+         </div>
+         </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
 </div>
 </div>
 </section>
